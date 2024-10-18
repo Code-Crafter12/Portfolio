0661ca208201bcfc69d281e6484d809e804dbfdf
Iaimen <aimanmumtaz743@gmail.com>
2024-10-17 21:28:15 -0700

    committted

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h6ggaky4t3ov)4#=hs+6cwdg82h*f6zgpe1f-#5itpui55o+hr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
